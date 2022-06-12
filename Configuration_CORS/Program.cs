a5d89f4e1f82d622f0f7bbfc07fa801236938a1e
GreedNeSS <ruslan199126@gmail.com>
2022-06-12 22:58:02 +0300

    Added Configuration CORS example.

--- Configuration_CORS/Program.cs
+++ Configuration_CORS/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(builder =>
+{
+    builder.WithOrigins("https://localhost:7150")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .WithExposedHeaders("custom-header");
+});
+
+app.MapGet("/", async (HttpContext context) =>
+{
+    context.Response.Headers.Add("custom-header", "custom header secret");
+    await context.Response.WriteAsync("Data Set");
+});
+
+app.MapGet("/login", async (HttpContext context) =>
+{
+    string? login = context.Request.Cookies["login"];
+    await context.Response.WriteAsync($"Login: {login}");
+});
 
 app.Run();
---
+++ Configuration_static_server/wwwroot/App.js
@@ -0,0 +1,31 @@
+﻿const btnLogin = document.querySelector("#loginBtn");
+const btnData = document.querySelector("#dataBtn");
+const result = document.querySelector("#data");
+const login = document.querySelector("#login");
+const header = document.querySelector("#header");
+
+btnData.addEventListener("click", async () => {
+    try {
+        const response = await fetch("https://localhost:7264");
+
+        if (response.ok) {
+            const headerTitle = "custom-header";
+            result.innerText = await response.text();
+            if (response.headers.has(headerTitle)) {
+                header.innerText = response.headers.get(headerTitle);
+            }
+        }
+    } catch (e) {
+        result.innerText = e.massage;
+    }
+});
+
+btnLogin.addEventListener("click", async () => {
+    try {
+        document.cookie = "login=greedness";
+        const response = await fetch("https://localhost:7264/login", { credentials: "include" });
+        if (response.ok) login.innerText = await response.text();
+    } catch (e) {
+        result.innerText = e.message;
+    }
+});
