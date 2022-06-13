ba371fb58c684bb274a56bbb2955fcb5d1e9fa33
GreedNeSS <ruslan199126@gmail.com>
2022-06-13 22:48:57 +0300

    Added examples.

--- CORS_Policy_Server/Program.cs
+++ CORS_Policy_Server/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Localhost7169", builder =>
+    {
+        builder.WithOrigins("https://localhost:7169")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("Localhost7169");
+
+app.MapGet("/", () => Results.Json(new { message = "CORS Policy" }));
 
 app.Run();
