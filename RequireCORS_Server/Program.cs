4e84a76c0718ddf034bd52d68e46fc7ee191bc34
GreedNeSS <ruslan199126@gmail.com>
2022-06-14 22:59:41 +0300

    Addded RequireCors example.

--- RequireCORS_Server/Program.cs
+++ RequireCORS_Server/Program.cs
@@ -1,6 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("localhost:7257", builder =>
+    {
+        builder.WithOrigins("https://localhost:7257")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+    options.AddPolicy("Production", builder =>
+    {
+        builder.WithOrigins("https://www.example.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+
+app.MapGet("/", async context => await context.Response.WriteAsync("Main Page"))
+    .RequireCors("localhost:7257");
+
+app.MapGet("/home", async context => await context.Response.WriteAsync("Home Page"))
+    .RequireCors("Production");
+
+app.MapGet("/about", async context => await context.Response.WriteAsync("About Page"))
+    .RequireCors(options => options.AllowAnyOrigin());
+
+app.MapGet("/contacts", async context => await context.Response.WriteAsync("Contacts Page"));
 
 app.Run();
---
+++ RequireCORS_Static_Server/wwwroot/App.js
@@ -0,0 +1,15 @@
+﻿const result = document.querySelector("#result");
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", async () => {
+    try {
+        const response = await fetch("https://localhost:7148/");
+
+        if (response.ok) {
+            resp = await response.json();
+            result.innerText = resp.message;
+        }
+    } catch (e) {
+        result.innerText = e.massage;
+    }
+})
